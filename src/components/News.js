e36e17551af59f65b4fab47db61d99c1ac5ca6e9
Vinay Homkar <homkar2015@gmail.com>
2023-11-08 16:14:00 +0530

    spinner-commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor(){
@@ -8,59 +9,63 @@ export class News extends Component {
       articles : [],
       loading : false,
       page : 1
-    }
+    };
   }
   async componentDidMount(){
     
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=39674f5c32724f34b8a961b6e275aaf8&page=1pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=39674f5c32724f34b8a961b6e275aaf8&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
-    this.setState({articles:parsedData.article, totalResults:parsedData.totalResults})
+    this.setState({articles:parsedData.articles, totalResults:parsedData.totalResults, loading:false})
   }
 
  handlePrevClick = async ()=>{
-    console.log("Previous")
-    let url =  `https://newsapi.org/v2/top-headlines?country=in&apiKey=39674f5c32724f34b8a961b6e275aaf8 ${this.state.page-1}&pageSize=20`;
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({
-      page:this.state.page-1,
-      articles:parsedData.articles
-    })
-    
-  }
-   handleNextClick =async ()=>{
-    console.log("Next")
-    if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+  console.log("previous")
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=39674f5c32724f34b8a961b6e275aaf8&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+  this.setState({loading:true});
+  let data = await fetch(url);
+  let parsedData = await data.json()
+  console.log(parsedData);
+  this.setState({
+    page:this.state.page-1,
+    articles:parsedData.articles,
+    loading:false
+  })
+}
+  handleNextClick = async ()=>{
+    console.log("next")
+    if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
 
-    }
-    else{
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=39674f5c32724f34b8a961b6e275aaf8 ${this.state.page+1}&pageSize=20`;
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=39674f5c32724f34b8a961b6e275aaf8&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
       let data = await fetch(url);
       let parsedData = await data.json()
-      console.log(parsedData);
+      
       this.setState({
         page:this.state.page+1,
-        articles:parsedData.articles
+        articles:parsedData.articles,
+        loading :false
       })
     }
   }
+  
   render() {
     return (
       <div className='container my-3'>
         <h1>Breaking News</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key = {element.url}>
+          {!this.state.loading && this.state.articles.map((element)=>{
+            return <div className="col-md-4" key = {element.url} >
               <NewsItem  title = {element.title?element.title:""} description = {element.description?element.description:""} imageUrl = {element.urlToImage} newsUrl = {element.url}/>
             </div>
           })}
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous </button>
-          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
